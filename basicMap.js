4d8e1b96f3c1549a2bc574fb38d55bdb9a872489
Palak <palakagarwal1237@gmail.com>
2022-02-16 09:46:02 +0530

    feat: add conditions based on testing

--- basicMap.js
+++ basicMap.js
@@ -1,2 +1,6 @@
-let doubleAll = (numbers) => numbers.map(number=>number*2);
+let doubleAll = (numbers=[]) => {
+    if (!numbers.isArray){
+        throw new Error ('Input is not an array');
+    }
+    return numbers.map(number=>number*2);};
 module.exports = doubleAll;
